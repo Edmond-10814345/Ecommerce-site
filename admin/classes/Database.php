05d9b85d6e68e19c2aaf7dc1ebc2a88f461c699a
Edmond-10814345 <82452815+Edmond-10814345@users.noreply.github.com>
2021-06-04 16:30:31 +0000

    Update Database.php

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "store_db");
+		$this->con = new Mysqli("us-cdbr-east-04.cleardb.com", "ba375438d13dc7", "226380d0", "heroku_559c85e9e0aeee8");
 		return $this->con;
 	}
 }
