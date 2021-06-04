dff8c59063534af2839531d20cbf57a6cbabb842
Edmond-10814345 <82452815+Edmond-10814345@users.noreply.github.com>
2021-06-04 14:07:47 +0000

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE_NAME', 'store_db');
+define('HOST', 'us-cdbr-east-04.cleardb.com');
+define('USER', 'ba375438d13dc7');
+define('PASSWORD', '226380d0');
+define('DATABASE_NAME', 'heroku_559c85e9e0aeee8');
 
 define('CURRENCY', '$');
 
